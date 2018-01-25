e08a5b650e3923c40dd6170e513728bb8aea745f
Sven Grunewaldt <strayer@olle-orks.org>
2018-01-25 20:24:26 +0100

    Make uid/gid of container user configurable

--- create-user.sh
+++ create-user.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
-set -e
+#!/usr/bin/env sh
+set -eu
 
 echo "Creating user $1 with home $2…"
 
-groupadd -g 1000 -r "$1"
-useradd -u 1000 -r -g "$1" -d "$2" -s /sbin/nologin -c "Docker image user" "$1"
+groupadd -g "$CONTAINER_USER_GID" -r "$1"
+useradd -u "$CONTAINER_USER_UID" -r -g "$1" -d "$2" -s /sbin/nologin -c "Docker image user" "$1"
