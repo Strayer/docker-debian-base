735df69fe9a512675872f56f97d0de67bd9ab176
Sven Grunewaldt <strayer@olle-orks.org>
2018-03-14 11:31:42 +0100

    debian-base: Add managed apt sources for APT_PROXY
    apt-update copies *.list files in /docker/sources.list.d to /etc/apt/sources.list.d and sets APT_PROXY remaps for known repositories.

--- debian-base/apt-update.sh
+++ debian-base/apt-update.sh
@@ -4,6 +4,11 @@ set -e
 # Always use default stretch sources.list
 cp /config/sources.list-stretch /etc/apt/sources.list
 
+# Copy over additional sources
+if test -n "$(find /docker/sources.list.d -maxdepth 1 -name '*.list' -print -quit 2>/dev/null)"; then
+  cp /docker/sources.list.d/*.list /etc/apt/sources.list.d/
+fi
+
 # Local Hetzner repositories (not publicly accessible)
 case $ENABLE_HETZNER_REPO in
   1|true)
@@ -19,7 +24,18 @@ case $APT_PROXY in
     cleaned_apt_proxy=`echo ${APT_PROXY} | sed "s:/*$:/:"`
     echo "Using apt proxy: $cleaned_apt_proxy"
     sed -i "s_deb[[:space:]]*http://_deb ${cleaned_apt_proxy}_" /etc/apt/sources.list
-    cat /etc/apt/sources.list
+
+    # put APT_PROXY in docker managed sources.list.d lists
+    # changing other source lists will cause them to not reset when run without
+    # APT_PROXY later, since only those in /docker/sources.list.d always get
+    # reset when this script runs
+    if test -n "$(find /etc/apt/sources.list.d -maxdepth 1 -name '*.list' -print -quit)"; then
+      for file in /etc/apt/sources.list.d/*.list; do
+        if test -e /docker/sources.list.d/${file##*/}; then
+          sed -r -i "s;deb[[:space:]]*https?://packages.sury.org/php/?;deb ${cleaned_apt_proxy}cached-sury_php;" $file
+        fi
+      done
+    fi
   ;;
 esac
 
