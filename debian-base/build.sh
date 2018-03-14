6405bad9d1e18d475e64f529c70fd2701be86ca1
Sven Grunewaldt <strayer@olle-orks.org>
2018-03-14 23:13:17 +0100

    debian-base: Enable Docker content trust

--- debian-base/build.sh
+++ debian-base/build.sh
@@ -3,9 +3,13 @@ set -e
 
 repository="strayer/debian-base"
 
+export DOCKER_CONTENT_TRUST=1
+
 docker build . --pull --no-cache \
     --build-arg ENABLE_HETZNER_REPO=$ENABLE_HETZNER_REPO \
     --build-arg APT_PROXY=$APT_PROXY \
     -t $repository:latest
+
+unset DOCKER_CONTENT_TRUST
 
 docker push $repository:latest
