305e790bdb7146218a3ba74377d362ebc14c2909
Sven Grunewaldt <strayer@olle-orks.org>
2018-03-29 12:47:18 +0200

    Reduce base image size
    Builds only use curl now, GPG keys are always added with COPY and
    verified with gpgv. Fixed default container user UID for sector001.

--- debian-base/gpg-recv-key.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-gpg --keyserver ha.pool.sks-keyservers.net --keyserver-options timeout=5 --recv-keys "$1" || \
-        gpg --keyserver pgp.mit.edu --keyserver-options timeout=5 --recv-keys "$1" || \
-        gpg --keyserver keyserver.pgp.com --keyserver-options timeout=5 --recv-keys "$1"
